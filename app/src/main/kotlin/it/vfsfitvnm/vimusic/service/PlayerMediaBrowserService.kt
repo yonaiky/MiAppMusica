29579778a01aa6b3669c84cfd3f1edd3cf7066fd
Rino Russo <rinorusso@gmail.com>
2024-03-15 18:43:01 +0100

    Implemented all playlists library in Android Auto, include ondevice songs. #628 #322

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/service/PlayerMediaBrowserService.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/service/PlayerMediaBrowserService.kt
@@ -399,7 +399,7 @@ class PlayerMediaBrowserService : MediaBrowserServiceCompat(), ServiceConnection
             val data = mediaId?.split('/') ?: return
             var index = 0
 
-            println("RiMusicMediaBrowser data $data ")
+            //println("RiMusicMediaBrowser data $data ")
 
             coroutineScope.launch {
                 val mediaItems = when (data.getOrNull(0)) {
