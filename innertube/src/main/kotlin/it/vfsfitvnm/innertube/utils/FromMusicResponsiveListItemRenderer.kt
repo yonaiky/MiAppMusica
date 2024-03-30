80a9e439351e2f07c14334813415bec9ea01bec4
Rino Russo <rinorusso@gmail.com>
2024-03-30 18:18:29 +0100

    Fixed load indian resources #1311

--- innertube/src/main/kotlin/it/vfsfitvnm/innertube/utils/FromMusicResponsiveListItemRenderer.kt
+++ innertube/src/main/kotlin/it/vfsfitvnm/innertube/utils/FromMusicResponsiveListItemRenderer.kt
@@ -35,24 +35,23 @@ fun Innertube.SongItem.Companion.from(renderer: MusicResponsiveListItemRenderer)
             ?.text
             ?.runs
             ?.getOrNull(0)
-            ?.let(Innertube::Info),
-            /*
             ?.let {
                 if (it.navigationEndpoint?.endpoint is NavigationEndpoint.Endpoint.Watch) Innertube.Info(
                     name = it.text,
                     endpoint = it.navigationEndpoint.endpoint as NavigationEndpoint.Endpoint.Watch
                 ) else null
             },
-             */
+            //?.let(Innertube::Info),
+
         authors = renderer
             .flexColumns
             .getOrNull(1)
             ?.musicResponsiveListItemFlexColumnRenderer
             ?.text
             ?.runs
-            ?.map<Runs.Run, Innertube.Info<NavigationEndpoint.Endpoint.Browse>>(Innertube::Info)
-            //?.map { Innertube.Info(name = it.text, endpoint = it.navigationEndpoint?.endpoint) }
-            //?.filterIsInstance<Innertube.Info<NavigationEndpoint.Endpoint.Browse>>()
+            //?.map<Runs.Run, Innertube.Info<NavigationEndpoint.Endpoint.Browse>>(Innertube::Info)
+            ?.map { Innertube.Info(name = it.text, endpoint = it.navigationEndpoint?.endpoint) }
+            ?.filterIsInstance<Innertube.Info<NavigationEndpoint.Endpoint.Browse>>()
             ?.takeIf(List<Any>::isNotEmpty),
         durationText = renderer
             .fixedColumns
