603f59e4157a263598ef9b87655aa9351e942a62
Rino Russo <rinorusso@gmail.com>
2024-04-05 13:16:48 +0200

    Implemented top and bottom navigation bar #852 #82

--- app/src/main/kotlin/it/fast4x/rimusic/ui/components/ScaffoldTB.kt
+++ app/src/main/kotlin/it/fast4x/rimusic/ui/components/ScaffoldTB.kt
@@ -15,10 +15,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.NavigationBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
