dbc4c148b155b4451f0939f0b85ac9fc6462f2a6
aneesh1122 <aneeshdohare@gmail.com>
2024-07-02 00:06:17 +0530

    added lyrics highlight

--- app/src/main/kotlin/it/fast4x/rimusic/utils/Preferences.kt
+++ app/src/main/kotlin/it/fast4x/rimusic/utils/Preferences.kt
@@ -210,6 +210,7 @@ const val thumbnailTypeKey = "thumbnailType"
 const val showvisthumbnailKey = "showvisthumbnail"
 const val extraspaceKey = "extraspace"
 const val expandedlyricsKey = "expandedlyrics"
+const val lyricshighlightKey = "lyricshighlight"
 /**** CUSTOM THEME **** */
 
 const val parentalControlEnabledKey = "parentalControlEnabled"
