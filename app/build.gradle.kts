84176318108e5860c84baf8754e4b8b7fd5161c0
Rino Russo <rinorusso@gmail.com>
2024-07-04 19:09:49 +0200

    General improvements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -158,29 +158,6 @@ dependencies {
     implementation(projects.lrclib)
     implementation(projects.piped)
 
-    //val appcompatVersion = "1.7.0"
-    //implementation("androidx.appcompat:appcompat:$appcompatVersion")
-    //implementation("androidx.appcompat:appcompat-resources:$appcompatVersion")
-    //implementation("androidx.core:core-splashscreen:1.0.1")
-    //implementation("androidx.media3:media3-datasource-okhttp:1.3.1")
-    //implementation("androidx.media:media:1.7.0")
-    //implementation("androidx.compose.material:material:1.6.7")
-    //implementation("androidx.compose.material3:material3-android:1.3.0-beta02")
-    //implementation("androidx.compose.ui:ui-graphics-android:1.6.7")
-    //implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    //implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
-    //implementation("androidx.core:core-ktx:1.13.1")
-    //implementation("androidx.compose.animation:animation:1.6.7")
-    //implementation("com.github.therealbush:translator:1.0.2")
-    //implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.9.3")
-    //implementation("com.github.KieronQuinn:MonetCompat:0.4.1")
-    //implementation("androidx.palette:palette:1.0.0")
-    //implementation("com.google.android.material:material:1.12.0")
-    //implementation("androidx.navigation:navigation-compose:2.7.7")
-    //implementation("io.github.vincent-series:smart-toast:4.1.6")
-    //implementation("com.jakewharton.timber:timber:5.0.1")
-    //implementation("androidx.security:security-crypto:1.1.0-alpha06")
-    
-    //End
+
     coreLibraryDesugaring(libs.desugaring)
 }
