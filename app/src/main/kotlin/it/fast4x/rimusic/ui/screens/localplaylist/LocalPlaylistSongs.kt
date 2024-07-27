505c666bf8b9fafd8dbc83e081801a868f90b278
martkol <55714786+martkol@users.noreply.github.com>
2024-07-27 13:28:42 +0200

    Update LocalPlaylistSongs.kt
    width and height of the icon entered independently.

--- app/src/main/kotlin/it/fast4x/rimusic/ui/screens/localplaylist/LocalPlaylistSongs.kt
+++ app/src/main/kotlin/it/fast4x/rimusic/ui/screens/localplaylist/LocalPlaylistSongs.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -1490,9 +1491,10 @@ fun LocalPlaylistSongs(
                                     reorderingState = reorderingState,
                                     index = index
                                 )
-                                .size(24.dp)
+                                .width(44.dp)
+                                .height(24.dp)
                                 .align(Alignment.TopEnd)
-                                .offset(x = -15.dp)
+                                .offset(x = -0.dp)
                                 .zIndex(10f)
                         )
                     }
@@ -1712,10 +1714,4 @@ fun LocalPlaylistSongs(
 
     }
 }
-
-
-
-
-
-
 
