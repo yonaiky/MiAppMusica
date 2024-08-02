6bc8573a6e1a9ec322c1ab37d0e51f3fca8d6065
Rino Russo <rinorusso@gmail.com>
2024-08-02 17:32:36 +0200

    General improvements

--- createapks.bat
+++ createapks.bat
@@ -1,2 +1,2 @@
 del app\release\app-release.apks
-java -jar ./bundletool.jar build-apks --ks="./APK keystore/RiMusicKeystore.jks" --ks-key-alias=key0 --bundle=./app/release/app-release.aab --output=./app/release/app-release.apks
+java -jar ./bundletool.jar build-apks --ks="./APK keystore/RiMusicKeystore.jks" --ks-key-alias=key0 --bundle=./app/accrescent/release/app-accrescent-release.aab --output=./app/accrescent/release/app-accrescent-release.apks
