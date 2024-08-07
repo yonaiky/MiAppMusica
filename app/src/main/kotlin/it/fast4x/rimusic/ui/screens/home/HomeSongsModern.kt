fdb8630ee9530985ca6f1b7f932ee2bbeac10d3c
Rino Russo <rinorusso@gmail.com>
2024-08-07 16:37:36 +0200

    Implemented system back handler in ondevice songs #1404

--- app/src/main/kotlin/it/fast4x/rimusic/ui/screens/home/HomeSongsModern.kt
+++ app/src/main/kotlin/it/fast4x/rimusic/ui/screens/home/HomeSongsModern.kt
@@ -7,6 +7,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.AnimatedVisibility
@@ -1283,6 +1284,13 @@ fun HomeSongsModern(
                 } else {
                     if (showFolders) {
                         if (currentFolderPath != "/") {
+
+                            item {
+                                BackHandler(onBack = {
+                                    currentFolderPath = currentFolderPath.removeSuffix("/").substringBeforeLast("/") + "/"
+                                })
+                            }
+
                             itemsIndexed(items = listOf(backButtonFolder)) { index, folderItem ->
                                 FolderItem(
                                     folder = folderItem,
