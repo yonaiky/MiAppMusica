f1b57c9febdd2da055cd106116e913cbf6ce76a3
Rino Russo <rinorusso@gmail.com>
2024-09-17 11:11:31 +0200

    General improvements

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -48,12 +48,12 @@ kotlin {
 
         androidMain.dependencies { }
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
+            //implementation(compose.runtime)
+            //implementation(compose.foundation)
+            //implementation(compose.material3)
+            //implementation(compose.ui)
             implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
+            //implementation(compose.components.uiToolingPreview)
         }
     }
 }
