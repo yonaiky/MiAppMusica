22ee90ac65ba3d2be661348d66aecee086c2dc22
martkol <55714786+martkol@users.noreply.github.com>
2024-09-20 20:14:23 +0200

    Update and rename loadingScreen.js to additional.js
    add events for languageselect creation

--- docs/js/additional.js
+++ docs/js/additional.js
@@ -6,6 +6,11 @@ class LoadingScreen {
       loadingScreen.style.display = "none";
     }, 400);
   }
+  static unload(){
+    const loadingScreen = document.querySelector(".loadingScreen");
+    loadingScreen.style.opacity = "1";
+    loadingScreen.style.display = "flex";
+  }
 }
 // Dark Mode
 const preference = localStorage.getItem("dark");
@@ -23,6 +28,16 @@ function toggleDark() {
   document.body.classList.toggle("dark");
   localStorage.setItem("dark", !dark);
 }
+// Detect Touch to prevent colouring Dark/Light Button
 function detecttouch() {
   document.getElementById("toggledark").classList.remove("focustoggle");
+}
+
+document.onmousedown = function (e) {
+  multilingual.langEvent(e)
+}
+window.onhashchange = function (e) {
+  if (document.location.hash == "#footer")  {
+    document.body.classList.remove("visible");
+  }
 }
