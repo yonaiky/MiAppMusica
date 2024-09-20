a5772dc420addb532e0f9da804887db575fa779e
martkol <55714786+martkol@users.noreply.github.com>
2024-09-20 20:32:39 +0200

    Create reveal.js

---
+++ docs/js/reveal.js
@@ -0,0 +1,17 @@
+var revealObjects
+async function reveal(){
+    revealObjects = document.getElementsByClassName("is-revealing")
+    for (const reveal of revealObjects){
+       boundings =  reveal.parentElement.getBoundingClientRect(); 
+       
+       if (boundings.height - boundings.top < -200  )
+        reveal.classList.remove("visible")
+        reveal.classList.add("invisible")
+
+       if ((boundings.top - window.innerHeight) < (0-(boundings.height) * 0.5)){
+            reveal.classList.add("visible")
+            reveal.classList.remove("invisible")
+       }
+    }
+}
+window.onscroll = function (e) {reveal()}
