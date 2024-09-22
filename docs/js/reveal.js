d5cbac77914051e1cb2dac8ececb06955ccde713
martkol <55714786+martkol@users.noreply.github.com>
2024-09-22 16:55:41 +0200

    Update reveal.js
    codeoptimation

--- docs/js/reveal.js
+++ docs/js/reveal.js
@@ -1,17 +1,17 @@
 var revealObjects
-async function reveal(){
+async function reveal() {
     revealObjects = document.getElementsByClassName("is-revealing")
-    for (const reveal of revealObjects){
-       boundings =  reveal.parentElement.getBoundingClientRect(); 
-       
-       if (boundings.height - boundings.top < -200  )
-        reveal.classList.remove("visible")
-        reveal.classList.add("invisible")
-
-       if ((boundings.top - window.innerHeight) < (0-(boundings.height) * 0.35)){
-            reveal.classList.add("visible")
-            reveal.classList.remove("invisible")
-       }
+    for (let a = 0; a < revealObjects.length; a++) {
+        boundings = revealObjects[a].parentElement.getBoundingClientRect();
+        if ((boundings.height - boundings.top < -200) && (boundings.top - window.innerHeight) > (0 - (boundings.height) * 0.35)) {
+            revealObjects[a].classList.remove("visible")
+            revealObjects[a].classList.add("invisible")
+            return;
+        }
+        revealObjects[a].classList.add("visible")
+        revealObjects[a].classList.remove("invisible")
     }
 }
-window.onscroll = function (e) {reveal()}
+window.onscroll = function (e) {
+    reveal()
+}
