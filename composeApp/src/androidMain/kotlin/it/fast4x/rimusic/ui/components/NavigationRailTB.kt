49bfbf0b75ccce1d34ad4098e18b5d6c46259be9
knighthat <knightchanelgaming@gmail.com>
2024-09-24 10:59:45 -0500

    enforce non-null, non-default args

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/NavigationRailTB.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/NavigationRailTB.kt
@@ -59,17 +59,17 @@ inline fun NavigationRailTB(
     navController: NavController,
     topIconButtonId: Int,
     noinline onTopIconButtonClick: () -> Unit,
-    showButton1: Boolean = true,
+    showButton1: Boolean,
     topIconButton2Id: Int,
     noinline onTopIconButton2Click: () -> Unit,
     showButton2: Boolean,
-    bottomIconButtonId: Int? = R.drawable.search,
+    bottomIconButtonId: Int?,
     noinline onBottomIconButtonClick: () -> Unit,
-    showBottomButton: Boolean = false,
+    showBottomButton: Boolean,
     tabIndex: Int,
     crossinline onTabIndexChanged: (Int) -> Unit,
-    content: @Composable() (ColumnScope.(@Composable (Int, String, Int) -> Unit) -> Unit),
-    hideTabs: Boolean = false,
+    content: @Composable (ColumnScope.(@Composable (Int, String, Int) -> Unit) -> Unit),
+    hideTabs: Boolean,
     modifier: Modifier = Modifier
 ) {
     val (colorPalette, typography, thumbnailShape) = LocalAppearance.current
