636c0584e4172d4bef5302484ce10c0e9121a50d
knighthat <knightchanelgaming@gmail.com>
2024-10-15 10:41:49 -0500

    assigned default values to enums

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/SortOrder.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/SortOrder.kt
@@ -1,8 +1,8 @@
 package it.fast4x.rimusic.enums
 
-enum class SortOrder {
-    Ascending,
-    Descending;
+enum class SortOrder( val rotationZ: Float ) {
+    Ascending( 0f ),
+    Descending( 180f );
 
     operator fun not() = when (this) {
         Ascending -> Descending
---
+++ composeApp/src/androidMain/kotlin/me/knighthat/enums/MenuTitle.kt
@@ -0,0 +1,6 @@
+package me.knighthat.enums
+
+interface MenuTitle {
+
+    val titleId: Int
+}
