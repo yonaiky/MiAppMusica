eec26e97548bc4033e53c18969c97ff1511bf74c
knighthat <knightchanelgaming@gmail.com>
2024-10-15 18:08:43 -0500

    implemented MenuTitle for default titleId

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/PlaylistSongSortBy.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/PlaylistSongSortBy.kt
@@ -1,31 +1,28 @@
 package it.fast4x.rimusic.enums
 
-enum class PlaylistSongSortBy {
-    Album,
-    AlbumYear,
-    Artist,
-    ArtistAndAlbum,
-    DatePlayed,
-    PlayTime,
-    Position,
-    Title,
-    Duration,
-    DateLiked,
-    DateAdded;
+import it.fast4x.rimusic.R
+import me.knighthat.enums.MenuTitle
 
-    /*
-    val index: Int
-        get() = when (this) {
-            AlbumYear -> 0
-            Artist -> 1
-            DatePlayed -> 2
-            PlayTime -> 3
-            Position -> 4
-            Title -> 5
-            Duration -> 6
-            ArtistAndAlbum -> 7
-    }
-     */
+enum class PlaylistSongSortBy( val textId: Int ): MenuTitle {
+    Album( R.string.sort_album ),
+    AlbumYear( R.string.sort_album_year ),
+    Artist( R.string.sort_artist ),
+    ArtistAndAlbum( -1 ),
+    DatePlayed( R.string.sort_date_played ),
+    PlayTime( R.string.sort_listening_time ),
+    Position( R.string.sort_position ),
+    Title( R.string.sort_title ),
+    Duration( R.string.sort_duration ),
+    DateLiked( R.string.sort_date_liked ),
+    DateAdded( R.string.sort_date_added );
 
+    override val titleId: Int
+        get() {
+            // Due to the requirement of 2 separated strings,
+            // this needs to be handled separately
+            if( this == ArtistAndAlbum )
+                throw UnsupportedOperationException()
 
+            return this.textId
+        }
 }
