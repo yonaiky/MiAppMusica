9bafed4512b2d88fed18597d0d23dc1b5d4dcc1a
knighthat <knightchanelgaming@gmail.com>
2024-10-15 19:14:47 -0500

    introduce Drawable for visual representation of an enum value

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/PlaylistSongSortBy.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/PlaylistSongSortBy.kt
@@ -1,20 +1,40 @@
 package it.fast4x.rimusic.enums
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import it.fast4x.rimusic.R
+import me.knighthat.enums.Drawable
 import me.knighthat.enums.MenuTitle
 
-enum class PlaylistSongSortBy( val textId: Int ): MenuTitle {
-    Album( R.string.sort_album ),
-    AlbumYear( R.string.sort_album_year ),
-    Artist( R.string.sort_artist ),
-    ArtistAndAlbum( -1 ),
-    DatePlayed( R.string.sort_date_played ),
-    PlayTime( R.string.sort_listening_time ),
-    Position( R.string.sort_position ),
-    Title( R.string.sort_title ),
-    Duration( R.string.sort_duration ),
-    DateLiked( R.string.sort_date_liked ),
-    DateAdded( R.string.sort_date_added );
+enum class PlaylistSongSortBy(
+    @StringRes val textId: Int,
+    @DrawableRes val iconId: Int
+): MenuTitle, Drawable {
+
+    Album( R.string.sort_album, R.drawable.album ),
+
+    AlbumYear( R.string.sort_album_year, R.drawable.calendar ),
+
+    Artist( R.string.sort_artist, R.drawable.artist ),
+
+    ArtistAndAlbum( -1, R.drawable.artist ),
+
+    DatePlayed( R.string.sort_date_played, R.drawable.up_right_arrow ),
+
+    PlayTime( R.string.sort_listening_time, R.drawable.trending ),
+
+    Position( R.string.sort_position, R.drawable.position ),
+
+    Title( R.string.sort_title, R.drawable.text ),
+
+    Duration( R.string.sort_duration, R.drawable.time ),
+
+    DateLiked( R.string.sort_date_liked, R.drawable.heart ),
+
+    DateAdded( R.string.sort_date_added, R.drawable.time );
 
     override val titleId: Int
         get() {
@@ -25,4 +45,8 @@ enum class PlaylistSongSortBy( val textId: Int ): MenuTitle {
 
             return this.textId
         }
+
+    override val icon: Painter
+        @Composable
+        get() = painterResource( this.iconId )
 }
