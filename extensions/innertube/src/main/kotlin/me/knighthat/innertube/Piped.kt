01b57a7213798106804e0d690820041a1591d416
knighthat <knightchanelgaming@gmail.com>
2024-10-25 15:58:27 -0500

    resolve merge conflicts

--- extensions/innertube/src/main/kotlin/me/knighthat/innertube/Piped.kt
+++ extensions/innertube/src/main/kotlin/me/knighthat/innertube/Piped.kt
@@ -1,57 +1,132 @@
 package me.knighthat.innertube
 
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.BrowserUserAgent
-import io.ktor.client.plugins.compression.ContentEncoding
-import io.ktor.client.plugins.compression.brotli
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.defaultRequest
-import io.ktor.http.HttpHeaders
-import io.ktor.serialization.kotlinx.json.json
-import it.fast4x.innertube.utils.ProxyPreferences
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
-import java.net.InetSocketAddress
-import java.net.Proxy
+import io.ktor.client.call.NoTransformationFoundException
+import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.get
+import io.ktor.client.request.request
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.URLProtocol
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import okio.IOException
 
 object Piped {
 
-    val client = HttpClient(OkHttp) {
-        BrowserUserAgent()
+    // REGEX's
+    private val DOMAIN_NO_PATH_PATTERN = Regex( "https?://(.*?)(?=\\s\\||/.* \\|)" )
+    private val TLD_REGEX = Regex("([a-zA-Z0-9-]+\\.[a-zA-Z]{2,})$")
+    //
 
-        expectSuccess = true
+    private const val INSTANCES_GITHUB =
+        "https://raw.githubusercontent.com/wiki/TeamPiped/Piped-Frontend/Instances.md"
 
-        install(ContentNegotiation) {
-            @OptIn(ExperimentalSerializationApi::class)
-            json(Json {
-                ignoreUnknownKeys = true
-                explicitNulls = false
-                encodeDefaults = true
-            })
-        }
+    private lateinit var API_INSTANCES: Array<String>
+    private lateinit var UNREACHABLE_INSTANCES: MutableList<Regex>
 
-        install(ContentEncoding) {
-            brotli()
-        }
+    /**
+     * Extract domain name from a full length URL.
+     *
+     * For example, url **https://sub1.example.org/path/to/file** will
+     * return **sub1.example.org**
+     */
+    private fun getDomainName( url: String ) =
+        DOMAIN_NO_PATH_PATTERN.find(url)?.groups?.get(1)?.value ?: url
 
-        ProxyPreferences.preference?.let {
-            engine {
-                proxy = Proxy(
-                    it.proxyMode,
-                    InetSocketAddress(
-                        it.proxyHost,
-                        it.proxyPort
-                    )
-                )
-            }
-        }
+    fun blacklistUrl( url: String ) {
+        val domainName = getDomainName( url )
+        val tld = TLD_REGEX.find( domainName )?.value ?: domainName
 
-        defaultRequest {
-            url(scheme = "https", host ="pipedapi.tokhmi.xyz") {
-                headers.append( HttpHeaders.Accept, "*/*" )
-            }
-        }
+        /**
+         * This pattern is design to match all sub domains (whether it's there or not) of a domain.
+         *
+         * I.E. **_(?:[a-zA-Z0-9-]+\.)*example.org_** matches:
+         * - example.org
+         * - sub1.example.org
+         * - extra.sub1.example.org
+         */
+        val domainNameRegex = Regex("(?:[a-zA-Z0-9-]+\\.)*${tld.replace(".", "\\.")}")
+
+        if( !::UNREACHABLE_INSTANCES.isInitialized )
+            throw UninitializedPropertyAccessException( "Please initialize Piped instances with Piped#fetchPipedInstance()" )
+        else
+            UNREACHABLE_INSTANCES.add( domainNameRegex )
     }
 
+    suspend fun fetchPipedInstances() {
+        val response = HttpFetcher.CLIENT
+                                  .get( INSTANCES_GITHUB )
+                                  .bodyAsText()
+
+        API_INSTANCES = DOMAIN_NO_PATH_PATTERN.findAll( response )
+                                              .map { it.groups[1]?.value }
+                                              .filterNotNull()
+                                              .toList()
+                                              .toTypedArray()
+
+        // Reset unreachable urls
+        UNREACHABLE_INSTANCES = mutableListOf()
+    }
+
+    internal suspend inline fun <reified T> asyncMultiRequest(
+        method: HttpMethod,
+        endpoint: String,
+        crossinline body: (HttpRequestBuilder) -> Unit
+    ): T? = coroutineScope {
+        val reachableUrls = API_INSTANCES.filter {
+            for( regex in UNREACHABLE_INSTANCES )
+                if( regex.matches( it ) )
+                    return@filter false
+
+            true
+        }
+
+        val deferredResponses = reachableUrls.map { hostUrl ->
+            async {
+                try {
+                    val response = HttpFetcher.CLIENT.request {
+                        this.method = method
+                        url {
+                            protocol = URLProtocol.HTTPS
+                            host = "$hostUrl$endpoint"
+                        }
+                        body(this)
+                    }
+
+                    // Only accept successful responses
+                    if (response.status == HttpStatusCode.OK) {
+                        println("Fetch $hostUrl$endpoint returned code: ${response.status}:")
+                        println(response.bodyAsText().replace("\n", ""))
+
+                        response.body<T>()
+                    } else
+                        null
+
+                } catch ( e: Exception ) {
+                    when( e ) {
+                        /**
+                         * This is a failsafe + performance boost.
+                         * If a website returns unusable responses, it will
+                         * be blacklisted to prevent from future use.
+                         *
+                         * This in turn makes the time it takes to fetch
+                         * data in the future shorter because there's less
+                         * link to fetch, also less error to check.
+                         */
+                        is IOException,
+                        is NoTransformationFoundException -> {
+                            blacklistUrl( hostUrl)
+                            null
+                        }
+                        else -> throw e
+                    }
+                }
+            }
+        }
+
+        // Wait for the first non-null response
+        deferredResponses.firstNotNullOfOrNull { it.await() }
+    }
 }
--- extensions/innertube/src/main/kotlin/me/knighthat/innertube/request/Player.kt
+++ extensions/innertube/src/main/kotlin/me/knighthat/innertube/request/Player.kt
@@ -1,13 +1,14 @@
 package me.knighthat.innertube.request
 
-import io.ktor.client.call.body
-import io.ktor.client.request.get
+import io.ktor.http.HttpMethod
 import it.fast4x.innertube.utils.runCatchingNonCancellable
 import me.knighthat.innertube.Piped
 import me.knighthat.innertube.response.PlayerResponse
 
-suspend fun Piped.player(
-    videoId: String,
-) = runCatchingNonCancellable {
-    client.get("/streams/$videoId").body<PlayerResponse>()
-}
+suspend fun Piped.player( videoId: String ): Result<PlayerResponse?>? =
+    runCatchingNonCancellable {
+        asyncMultiRequest<PlayerResponse>(
+            HttpMethod.Get,
+            "/streams/$videoId"
+        ) {}
+    }
