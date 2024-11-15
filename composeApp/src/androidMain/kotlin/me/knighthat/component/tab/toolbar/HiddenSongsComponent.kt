8629deeb526b767d31df87fab18f384e0abb4e3f
knighthat <knightchanelgaming@gmail.com>
2024-11-14 20:49:55 -0600

    convert show hidden songs to class

---
+++ composeApp/src/androidMain/kotlin/me/knighthat/component/tab/toolbar/HiddenSongsComponent.kt
@@ -0,0 +1,39 @@
+package me.knighthat.component.tab.toolbar
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import it.fast4x.rimusic.R
+
+class HiddenSongsComponent private constructor(
+    private val showHiddenState: MutableState<Int>
+): ToggleableIcon, Descriptive {
+
+    companion object {
+        @JvmStatic
+        @Composable
+        fun init() = HiddenSongsComponent(
+            remember { mutableIntStateOf(0) }   // False equivalent
+        )
+    }
+
+    var showHidden: Int = showHiddenState.value
+        set(value) {
+            showHiddenState.value = value
+            field = value
+        }
+    override var isVisible: Boolean = showHidden == -1
+        set(value) {
+            // Clarification:
+            // showHidden == -1 means show hidden songs
+            // showHidden == 0 means hide them
+            showHidden = if( value ) -1 else 0
+            field = value
+        }
+    override val iconIdOff: Int = R.drawable.eye_off
+    override val iconId: Int = R.drawable.eye
+    override val textId: Int = R.string.info_show_hide_hidden_songs
+
+    override fun onShortClick() { isVisible = !isVisible }
+}
--- composeApp/src/androidMain/kotlin/me/knighthat/component/tab/toolbar/ToggleableIcon.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/component/tab/toolbar/ToggleableIcon.kt
@@ -1,6 +1,35 @@
 package me.knighthat.component.tab.toolbar
 
+import androidx.compose.runtime.Composable
+import me.knighthat.component.header.TabToolBar
+
 interface ToggleableIcon: Icon {
 
     var isVisible: Boolean
+    val iconIdOff: Int
+
+    @Composable
+    override fun ToolBarButton() {
+        if( this is Clickable )
+            TabToolBar.Toggleable(
+                iconId,
+                iconIdOff,
+                isVisible,
+                color,
+                sizeDp,
+                modifier,
+                this::onShortClick,
+                this::onLongClick
+            )
+        else
+            TabToolBar.Toggleable(
+                iconId,
+                iconIdOff,
+                isVisible,
+                color,
+                sizeDp,
+                modifier,
+                this::onShortClick
+            )
+    }
 }
