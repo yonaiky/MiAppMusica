403d8774fc1ad897229647461bd0549faa101c40
twistios <54811436+twistios@users.noreply.github.com>
2024-11-26 00:05:29 +0100

    Fixed a crash when using visualizer while offline #4643
    The app crashes if the visualizer is enabled, the device has no network connection (is offline) and the song is switched to a different song.
    
    - correct throwing of the error
    - correct catching of the error

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/utils/BitmapUtils.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/utils/BitmapUtils.kt
@@ -4,13 +4,18 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import coil.ImageLoader
+import coil.request.ErrorResult
 import coil.request.ImageRequest
 import coil.request.SuccessResult
 
 suspend fun getBitmapFromUrl(context: Context, url: String): Bitmap {
     val loading = ImageLoader(context)
     val request = ImageRequest.Builder(context).data(url).allowHardware(false).build()
-    val result = (loading.execute(request) as SuccessResult).drawable
-    return (result as BitmapDrawable).bitmap
+    val result = loading.execute(request)
+    if(result is ErrorResult) {
+        throw result.throwable
+    }
+    val drawable = (result as SuccessResult).drawable
+    return (drawable as BitmapDrawable).bitmap
 }
 
