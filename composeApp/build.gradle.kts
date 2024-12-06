6203f25d2b30e2c959ffcf204b3bf6c3383ec0b9
Rino Russo <rinorusso@gmail.com>
2024-12-06 18:58:47 +0100

    New release version 0.6.63

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -123,8 +123,8 @@ android {
         applicationId = "it.fast4x.rimusic"
         minSdk = 21
         targetSdk = 35
-        versionCode = 72
-        versionName = "0.6.62.2"
+        versionCode = 73
+        versionName = "0.6.63"
     }
 
     splits {
