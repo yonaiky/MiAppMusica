7f510a39ad99f9d9d179dbaf89cd047620a8d0a7
knighthat <contact@knighthat.me>
2024-12-07 01:15:24 -0600

    default sort order function

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/SortOrder.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/SortOrder.kt
@@ -1,5 +1,7 @@
 package it.fast4x.rimusic.enums
 
+import org.jetbrains.annotations.Contract
+
 enum class SortOrder( val rotationZ: Float ) {
     Ascending( 0f ),
     Descending( 180f );
@@ -8,4 +10,25 @@ enum class SortOrder( val rotationZ: Float ) {
         Ascending -> Descending
         Descending -> Ascending
     }
+
+    /**
+     * Attempt to apply sort order based on selected value.
+     *
+     * The provided list [items] is always assumed to be sorted
+     * in ascending order. Therefore, it only get reversed
+     * when [Descending] is selected.
+     *
+     * Return list is always a new list to prevent unwanted results
+     *
+     * @return a new list regardless selected value
+     */
+    @Contract(
+        value = "_->new",
+        pure = true
+    )
+    fun <T> applyTo( items: List<T> ): List<T> =
+        when( this ) {
+            Descending -> items.reversed()
+            Ascending -> items.toList()
+        }
 }
