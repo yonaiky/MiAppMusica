665aaf527dc7e2b0b3678881e68c52cc42922aab
Rino Russo <rinorusso@gmail.com>
2024-12-13 22:06:37 +0100

    General improvements

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/CustomModalBottomSheet.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/CustomModalBottomSheet.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContent
 import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -19,6 +22,7 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,10 +34,12 @@ import androidx.compose.ui.window.DialogWindowProvider
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import it.fast4x.rimusic.colorPalette
 import it.fast4x.rimusic.enums.ColorPaletteMode
 import it.fast4x.rimusic.utils.colorPaletteModeKey
 import it.fast4x.rimusic.utils.isLandscape
 import it.fast4x.rimusic.utils.rememberPreference
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -55,7 +61,44 @@ fun CustomModalBottomSheet(
 ) {
     val bottomPadding = if(isLandscape) 0.dp else WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
 
+    //NEW SHEET
+//    val mbSheetState: ModalBottomSheetState = androidx.compose.material.rememberModalBottomSheetState(
+//        initialValue = androidx.compose.material.ModalBottomSheetValue.Expanded,
+//        skipHalfExpanded = true,
+//        confirmValueChange = {
+//            if (it == androidx.compose.material.ModalBottomSheetValue.Hidden) {
+//                onDismissRequest()
+//            }
+//            true
+//        }
+//    )
+
     if (showSheet) {
+
+        //NEW SHEET
+//        val scope = rememberCoroutineScope()
+//        SideEffect {
+//            scope.launch {
+//                mbSheetState.show()
+//            }
+//        }
+//        ModalBottomSheetLayout(
+//            sheetState =  mbSheetState,
+//            sheetContentColor = colorPalette().background0,
+//            sheetBackgroundColor = colorPalette().background0,
+//            sheetShape = shape,
+//            sheetElevation = tonalElevation,
+//            scrimColor = scrimColor,
+//            sheetContent = {
+//                Column(Modifier.padding(bottom = bottomPadding)) {
+//                    content()
+//                }
+//            },
+//            content = {}
+//        )
+        // NEW SHEET
+
+        //PREVIOUS SHEET
         ModalBottomSheet(
             onDismissRequest = onDismissRequest,
             modifier = modifier,
@@ -87,5 +130,6 @@ fun CustomModalBottomSheet(
                 content()
             }
         }
+        //PREVIOUS SHEET
     }
 }
