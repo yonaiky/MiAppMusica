89e29aeda84e27a3e5570b4c76c310a71ca77f35
knighthat <knightchanelgaming@gmail.com>
2024-12-18 22:35:26 -0600

    serializable release data object

---
+++ composeApp/src/androidMain/kotlin/me/knighthat/updater/GithubRelease.kt
@@ -0,0 +1,35 @@
+package me.knighthat.updater
+
+import android.text.format.Formatter
+import it.fast4x.rimusic.appContext
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.time.Instant
+
+@Serializable
+data class GithubRelease(
+    val id: UInt,
+    @SerialName("tag_name") val tagName: String,
+    val name: String,
+    @SerialName("assets") val builds: List<Build>
+) {
+
+    @Serializable
+    data class Build(
+        val id: UInt,
+        val url: String,
+        val name: String,
+        val size: UInt,
+        @SerialName("created_at") val createdAt: String,
+        @SerialName("browser_download_url") val downloadUrl: String
+    ) {
+        val buildTime: Instant by lazy {
+            Instant.parse( createdAt )
+        }
+
+        val readableSize: String
+            // Don't remember with lazy because format mat be changed
+            // when [appContext] is changed.
+            get() = Formatter.formatShortFileSize( appContext(), this.size.toLong() )
+    }
+}
