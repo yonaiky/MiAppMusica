825bf990505ec97ddc2e22e8922a8f063ba27439
aneesh1122 <45353488+aneesh1122@users.noreply.github.com>
2024-12-25 19:19:29 +0530

    Added different gradient options

---
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/AnimatedGradient.kt
@@ -0,0 +1,16 @@
+package it.fast4x.rimusic.enums
+
+enum class AnimatedGradient {
+    Linear,
+    Mesh,
+    MesmerizingLens,
+    GlossyGradients,
+    GradientFlow,
+    PurpleLiquid,
+    InkFlow,
+    OilFlow,
+    IceReflection,
+    Stage,
+    GoldenMagma,
+    BlackCherryCosmos
+}
