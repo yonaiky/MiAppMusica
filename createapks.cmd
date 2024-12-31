a6fa94c46c393fcbe310df05a20394eb398f3a9b
knighthat <git@knighthat.me>
2024-12-31 10:22:25 -0600

    remove unused files

--- createapks.cmd
+++
@@ -1,2 +0,0 @@
-del composeApp\accrescent\release\composeApp-accrescent-release.apks
-java -jar ./bundletool.jar build-apks --ks="./APK keystore/RiMusicKeystore.jks" --ks-key-alias=key0 --bundle=./composeApp/accrescent/release/composeApp-accrescent-release.aab --output=./composeApp/accrescent/release/composeApp-accrescent-release.apks
