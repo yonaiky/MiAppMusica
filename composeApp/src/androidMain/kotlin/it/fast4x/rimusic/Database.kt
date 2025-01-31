4780a18a25a9fa589013da6f06f0eda47cbdd24f
twistios <54811436+twistios@users.noreply.github.com>
2025-01-31 14:36:00 +0100

    added missing import for `isExplicit`

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/Database.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/Database.kt
@@ -59,6 +59,7 @@ import it.fast4x.rimusic.models.SongPlaylistMap
 import it.fast4x.rimusic.models.SongWithContentLength
 import it.fast4x.rimusic.models.SortedSongPlaylistMap
 import it.fast4x.rimusic.service.LOCAL_KEY_PREFIX
+import it.fast4x.rimusic.utils.isExplicit
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import org.intellij.lang.annotations.MagicConstant
