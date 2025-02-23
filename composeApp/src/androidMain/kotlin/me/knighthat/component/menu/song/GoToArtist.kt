19c007ec92f95c6f30b866ff76bdce44e723011b
knighthat <git@knighthat.me>
2025-02-22 22:37:22 -0600

    fetch artist's id if not stored in the database

--- composeApp/src/androidMain/kotlin/me/knighthat/component/menu/song/GoToArtist.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/component/menu/song/GoToArtist.kt
@@ -2,17 +2,22 @@ package me.knighthat.component.menu.song
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import it.fast4x.innertube.Innertube
 import it.fast4x.rimusic.Database
 import it.fast4x.rimusic.R
 import it.fast4x.rimusic.appContext
 import it.fast4x.rimusic.enums.NavRoutes
+import it.fast4x.rimusic.enums.PopupType
+import it.fast4x.rimusic.models.Artist
 import it.fast4x.rimusic.models.Song
 import it.fast4x.rimusic.ui.components.tab.toolbar.Descriptive
 import it.fast4x.rimusic.ui.components.tab.toolbar.MenuIcon
+import it.fast4x.rimusic.ui.components.themed.SmartMessage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.guava.future
+import timber.log.Timber
 
 class GoToArtist private constructor(
     private val navController: NavController,
@@ -35,9 +40,34 @@ class GoToArtist private constructor(
     override fun onShortClick() {
         val song = getSong()
 
-        // TODO: Add a fetch function to get artists' info if it's not in the database
         CoroutineScope( Dispatchers.IO ).future {
-            Database.findArtistOfSong( song.id ).firstOrNull()
+            var result = Database.findArtistOfSong( song.id ).firstOrNull()
+
+            // If artist isn't stored inside database, attempt to fetch
+            if( result == null ) {
+                SmartMessage(
+                    message = appContext().getString( R.string.looking_up_artist_online, song.artistsText ),
+                    context = appContext()
+                )
+
+                Innertube.player(videoId = song.id)
+                         .onSuccess { response ->
+                             response.second
+                                     ?.videoDetails
+                                     ?.channelId
+                                     ?.let { result = Artist(it) }
+                         }.onFailure {
+                             Timber.tag("go_to_artist").e(it)
+
+                             SmartMessage(
+                                 message = appContext().getString( R.string.failed_to_fetch_artist ),
+                                 context = appContext(),
+                                 type = PopupType.Error
+                             )
+                         }
+            }
+
+            result
         }.get()?.let {
             navController.navigate(route = "${NavRoutes.artist.name}/${it.id}")
         }
