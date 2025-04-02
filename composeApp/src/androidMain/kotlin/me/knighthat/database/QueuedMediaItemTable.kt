34fa0a443c31f25af473ac07f7ed59ca758fab28
knighthat <git@knighthat.me>
2025-04-02 00:19:27 -0500

    lift QueuedMediaItemTable from SqlTable

--- composeApp/src/androidMain/kotlin/me/knighthat/database/QueuedMediaItemTable.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/database/QueuedMediaItemTable.kt
@@ -1,6 +1,8 @@
 package me.knighthat.database
 
+import android.database.SQLException
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.RewriteQueriesToDropUnusedColumns
 import it.fast4x.rimusic.models.QueuedMediaItem
@@ -8,7 +10,7 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 @RewriteQueriesToDropUnusedColumns
-interface QueuedMediaItemTable: SqlTable<QueuedMediaItem> {
+interface QueuedMediaItemTable {
 
     /**
      * @return all records from this table
@@ -19,6 +21,27 @@ interface QueuedMediaItemTable: SqlTable<QueuedMediaItem> {
         LIMIT :limit
     """)
     fun all( limit: Int = Int.MAX_VALUE ): Flow<List<QueuedMediaItem>>
+
+    /**
+     * Attempt to write the list of [QueuedMediaItem] to database.
+     *
+     * ### Standalone use
+     *
+     * When **1** element fails, the entire list is
+     * considered failed, database rolls back its operation,
+     * and passes exception to caller.
+     *
+     * ### Transaction use
+     *
+     * When **1** element fails, the entire list is
+     * considered failed, **the entire transaction rolls back**
+     * and passes exception to caller.
+     *
+     * @param queuedMediaItems list of [QueuedMediaItem] to insert to database
+     */
+    @Insert
+    @Throws(SQLException::class)
+    fun insert( queuedMediaItems: List<QueuedMediaItem> )
 
     @Query("DELETE FROM QueuedMediaItem")
     fun deleteAll(): Int
