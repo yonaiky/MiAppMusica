aae11d41006b57889fa45c71fccac47c81efa8f8
knighthat <git@knighthat.me>
2025-04-02 00:33:29 -0500

    lift SearchQueryTable from SqlTable

--- composeApp/src/androidMain/kotlin/me/knighthat/database/SearchQueryTable.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/database/SearchQueryTable.kt
@@ -1,6 +1,10 @@
 package me.knighthat.database
 
+import android.database.SQLException
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.RewriteQueriesToDropUnusedColumns
 import it.fast4x.rimusic.models.SearchQuery
@@ -8,7 +12,7 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 @RewriteQueriesToDropUnusedColumns
-interface SearchQueryTable: SqlTable<SearchQuery> {
+interface SearchQueryTable {
 
     /**
      * [searchTerm] appears in [SearchQuery.query].
@@ -25,6 +29,41 @@ interface SearchQueryTable: SqlTable<SearchQuery> {
         WHERE `query` LIKE '%' || :searchTerm || '%' COLLATE NOCASE
         """)
     fun findAllContain( searchTerm: String ): Flow<List<SearchQuery>>
+
+    /**
+     * Attempt to write [searchQuery] into database.
+     *
+     * ### Standalone use
+     *
+     * When error occurs and [SQLException] is thrown,
+     * it'll simply be ignored.
+     *
+     * ### Transaction use
+     *
+     * When error occurs and [SQLException] is thrown,
+     * it'll simply be ignored and the transaction continues.
+     *
+     * @param searchQuery intended to insert in to database
+     */
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertIgnore( searchQuery: SearchQuery )
+
+    /**
+     * Attempt to remove a record from database.
+     *
+     * @param searchQuery data intended to delete from database
+     */
+    @Delete
+    fun delete( searchQuery: SearchQuery )
+
+    /**
+     * Attempt to remove records from database.
+     *
+     * @param searchQuery list of [SearchQuery] to delete from database
+     * @return number of rows affected by the this operation
+     */
+    @Delete
+    fun delete( searchQuery: List<SearchQuery> ): Int
 
     @Query("DELETE FROM SearchQuery")
     fun deleteAll(): Int
