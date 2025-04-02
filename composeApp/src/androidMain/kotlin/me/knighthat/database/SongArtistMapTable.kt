0e337aec0e49b0f70d60d99995c5bc6a88a5fa44
knighthat <git@knighthat.me>
2025-04-02 00:26:02 -0500

    lift SongArtistMapTable from SqlTable

--- composeApp/src/androidMain/kotlin/me/knighthat/database/SongArtistMapTable.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/database/SongArtistMapTable.kt
@@ -1,8 +1,12 @@
 package me.knighthat.database
 
+import android.database.SQLException
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.RewriteQueriesToDropUnusedColumns
+import androidx.room.Upsert
 import it.fast4x.rimusic.models.Artist
 import it.fast4x.rimusic.models.Song
 import it.fast4x.rimusic.models.SongArtistMap
@@ -10,21 +14,38 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 @RewriteQueriesToDropUnusedColumns
-interface SongArtistMapTable: SqlTable<SongArtistMap> {
+interface SongArtistMapTable {
 
     /**
-     * Delete all mappings where songs aren't exist in `Song` table
+     * Attempt to write [songArtistMap] into database.
      *
-     * @return number of rows affected by this operation
+     * ### Standalone use
+     *
+     * When error occurs and [SQLException] is thrown,
+     * it'll simply be ignored.
+     *
+     * ### Transaction use
+     *
+     * When error occurs and [SQLException] is thrown,
+     * it'll simply be ignored and the transaction continues.
+     *
+     * @param songArtistMap data intended to insert in to database
      */
-    @Query("""
-        DELETE FROM SongArtistMap 
-        WHERE songId NOT IN (
-            SELECT DISTINCT id
-            FROM Song
-        )
-    """)
-    fun clearGhostMaps(): Int
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertIgnore( songArtistMap: SongArtistMap )
+
+    /**
+     * Attempt to write the list of [SongArtistMap] to database.
+     *
+     * If record exist (determined by its primary key),
+     * existing record's columns will be replaced
+     * by provided data.
+     *
+     * @param songArtistMap list of [SongArtistMap] to insert to database
+     * @return list of ROWID of successfully modified [SongArtistMap]
+     */
+    @Upsert
+    fun upsert( songArtistMap: List<SongArtistMap> ): List<Long>
 
     /**
      * @param artistId of artist to look for
@@ -41,4 +62,18 @@ interface SongArtistMapTable: SqlTable<SongArtistMap> {
         LIMIT :limit
     """)
     fun allSongsBy( artistId: String, limit: Int = Int.MAX_VALUE ): Flow<List<Song>>
+
+    /**
+     * Delete all mappings where songs aren't exist in `Song` table
+     *
+     * @return number of rows affected by this operation
+     */
+    @Query("""
+        DELETE FROM SongArtistMap 
+        WHERE songId NOT IN (
+            SELECT DISTINCT id
+            FROM Song
+        )
+    """)
+    fun clearGhostMaps(): Int
 }
