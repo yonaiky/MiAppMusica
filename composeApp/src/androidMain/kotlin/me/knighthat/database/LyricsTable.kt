0641bf63e70d8ec069ebf6d705d53b48052b3b31
knighthat <git@knighthat.me>
2025-04-02 00:03:17 -0500

    lift LyricsTable from SqlTable

--- composeApp/src/androidMain/kotlin/me/knighthat/database/LyricsTable.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/database/LyricsTable.kt
@@ -3,12 +3,13 @@ package me.knighthat.database
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.RewriteQueriesToDropUnusedColumns
+import androidx.room.Upsert
 import it.fast4x.rimusic.models.Lyrics
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 @RewriteQueriesToDropUnusedColumns
-interface LyricsTable: SqlTable<Lyrics> {
+interface LyricsTable {
 
     /**
      * @param songId of song to look for
@@ -16,4 +17,17 @@ interface LyricsTable: SqlTable<Lyrics> {
      */
     @Query("SELECT DISTINCT * FROM Lyrics WHERE songId = :songId")
     fun findBySongId( songId: String ): Flow<Lyrics?>
+
+    /**
+     * Attempt to write [lyrics] into database.
+     *
+     * If [lyrics] exist (determined by its primary key),
+     * existing record's columns will be replaced
+     * by provided [lyrics]' data.
+     *
+     * @param lyrics data intended to insert in to database
+     * @return ROWID of successfully modified record
+     */
+    @Upsert
+    fun upsert( lyrics: Lyrics ): Long
 }
