73a366eebc13026f3b4605512599cbd2198ec49a
knighthat <git@knighthat.me>
2025-04-17 10:07:09 -0500

    remove Relation

--- composeApp/src/androidMain/kotlin/me/knighthat/database/ext/FormatWithSong.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/database/ext/FormatWithSong.kt
@@ -1,15 +1,10 @@
 package me.knighthat.database.ext
 
 import androidx.room.Embedded
-import androidx.room.Relation
 import it.fast4x.rimusic.models.Format
 import it.fast4x.rimusic.models.Song
 
 data class FormatWithSong(
     @Embedded val format: Format,
-    @Relation(
-        parentColumn = "songId",
-        entityColumn = "id",
-    )
-    val song: Song
+    @Embedded val song: Song
 )
