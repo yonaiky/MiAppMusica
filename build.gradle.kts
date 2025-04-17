d12796979f31734ce1bf00e01c127c4e00a67479
knighthat <git@knighthat.me>
2025-04-17 10:56:05 -0500

    remove unused plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.ksp) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
