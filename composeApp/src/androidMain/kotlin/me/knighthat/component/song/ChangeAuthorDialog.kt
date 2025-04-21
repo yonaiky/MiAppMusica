1e42b750c99dd563636118c12d2afda27410167c
knighthat <git@knighthat.me>
2025-04-21 15:28:58 -0500

    add modified prefix to prevent reset on playback

--- composeApp/src/androidMain/kotlin/me/knighthat/component/song/ChangeAuthorDialog.kt
+++ composeApp/src/androidMain/kotlin/me/knighthat/component/song/ChangeAuthorDialog.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import app.kreate.android.R
 import it.fast4x.rimusic.Database
+import it.fast4x.rimusic.MODIFIED_PREFIX
 import it.fast4x.rimusic.models.Song
 import me.knighthat.component.RenameDialog
 import me.knighthat.utils.Toaster
@@ -54,7 +55,7 @@ class ChangeAuthorDialog private constructor(
 
         val song = getSong() ?: return
         Database.asyncTransaction {
-            songTable.updateArtists( song.id, newValue )
+            songTable.updateArtists( song.id, "$MODIFIED_PREFIX$newValue" )
             Toaster.done()
         }
 
