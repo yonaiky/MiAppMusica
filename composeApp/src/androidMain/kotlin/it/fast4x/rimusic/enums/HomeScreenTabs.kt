99b135a08c6e0fb042512fce03f7af15d070660c
knighthat <git@knighthat.me>
2025-06-04 20:07:39 -0500

    merge default values to constructor

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/HomeScreenTabs.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/HomeScreenTabs.kt
@@ -5,31 +5,21 @@ import app.kreate.android.R
 import me.knighthat.enums.TextView
 
 enum class HomeScreenTabs(
+    val index: Int,
     @field:StringRes override val textId: Int
 ): TextView {
 
-    Default( R.string._default ),
+    Default( 0, R.string._default ),
 
-    QuickPics( R.string.quick_picks ),
+    QuickPics( 0, R.string.quick_picks ),
 
-    Songs( R.string.songs ),
+    Songs( 1, R.string.songs ),
 
-    Artists( R.string.artists ),
+    Artists( 2, R.string.artists ),
 
-    Albums( R.string.albums ),
+    Albums( 3, R.string.albums ),
 
-    Playlists( R.string.playlists ),
+    Playlists( 4, R.string.playlists ),
 
-    Search( R.string.search );
-
-    val index: Int
-        get() = when (this) {
-            Default -> 100
-            QuickPics -> 0
-            Songs -> 1
-            Artists -> 2
-            Albums -> 3
-            Playlists -> 4
-            Search -> 5
-        }
+    Search( 5, R.string.search );
 }
