9d08588d3d06f9091fd417dffcfa8c0054875a85
knighthat <git@knighthat.me>
2025-06-12 00:41:31 -0500

    remove unnecessary @Composable

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/UiType.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/enums/UiType.kt
@@ -1,6 +1,5 @@
 package it.fast4x.rimusic.enums
 
-import androidx.compose.runtime.Composable
 import app.kreate.android.Settings
 
 enum class UiType {
@@ -12,9 +11,7 @@ enum class UiType {
         fun current(): UiType = Settings.MAIN_THEME.value
     }
 
-    @Composable
     fun isCurrent(): Boolean = current() == this
 
-    @Composable
     fun isNotCurrent(): Boolean = !isCurrent()
 }
