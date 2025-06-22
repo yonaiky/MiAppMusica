a3fa1b1172b97e4b4e63adb22bf9bf992a3f0315
knighthat <git@knighthat.me>
2025-06-21 21:05:04 -0500

    bump version from 1.5.0 to 1.5.1 (103)

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -126,8 +126,8 @@ android {
         applicationId = "me.knighthat.kreate"
         minSdk = 21
         targetSdk = 35
-        versionCode = 102
-        versionName = "1.5.0"
+        versionCode = 103
+        versionName = "1.5.1"
 
         /*
                 UNIVERSAL VARIABLES
