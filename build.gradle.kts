df113ba8682b6a0c8dcbe18b6164cb464fcf9f34
knighthat <git@knighthat.me>
2025-07-08 17:55:53 -0500

    add kotlin jvm to plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,5 @@ plugins {
     // Others
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.ksp) apply false
+    alias( libs.plugins.kotlin.jvm ) apply false
 }
