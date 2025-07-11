012173613479efb1f0c810203e368aa2b8d806ac
knighthat <git@knighthat.me>
2025-07-11 15:49:17 -0500

    squash! remove Piped module

--- extensions/piped/src/main/kotlin/it/fast4x/piped/utils/Coroutines.kt
+++
@@ -1,6 +0,0 @@
-package it.fast4x.piped.utils
-
-import kotlinx.coroutines.CancellationException
-
-inline fun <T> runCatchingCancellable(block: () -> T) =
-    runCatching(block).takeIf { it.exceptionOrNull() !is CancellationException }
