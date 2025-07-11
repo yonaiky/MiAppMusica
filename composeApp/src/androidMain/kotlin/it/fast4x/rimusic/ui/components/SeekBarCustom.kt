4c503a31763c4faf20ada0c3938ef71be22a8141
knighthat <git@knighthat.me>
2025-07-10 20:54:00 -0500

    fix square timeline at the start when PlayerTimelineType.BodiedBar is active

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/SeekBarCustom.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/ui/components/SeekBarCustom.kt
@@ -3,24 +3,23 @@ package it.fast4x.rimusic.ui.components
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.rememberTransition
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -151,22 +150,15 @@ fun SeekBarCustom(
             }
             //.height(scrubberRadius)
     ) {
-
-        Spacer(
-            modifier = Modifier
-                //.height(currentBarHeight)
-                .height(_backbarHeight)
-                .fillMaxWidth()
-                .background(color = backgroundColor, shape = shape)
-                .align(Alignment.Center)
-        )
-
-        Spacer(
-            modifier = Modifier
-                .height(currentBarHeight)
-                .fillMaxWidth((value.toFloat() - minimumValue) / (maximumValue - minimumValue))
-                .background(color = color, shape = shape)
-                .align(Alignment.CenterStart)
+        LinearProgressIndicator(
+            progress = { (value.toFloat() - minimumValue) / (maximumValue - minimumValue) },
+            modifier = Modifier.fillMaxWidth()
+                               .height( _backbarHeight )
+                               .background( backgroundColor, shape ),
+            color = color,
+            trackColor = Color.Transparent,
+            strokeCap = StrokeCap.Round,
+            gapSize = Dp.Hairline
         )
     }
 }
