df1e943324f7458817dc8a808752ab82c1cac75c
knighthat <git@knighthat.me>
2025-07-16 02:50:15 -0500

    show "check now" button when setting is disabled

--- composeApp/src/androidGithub/kotlin/app/kreate/android/themed/common/screens/settings/general/UpdateSection.kt
+++ composeApp/src/androidGithub/kotlin/app/kreate/android/themed/common/screens/settings/general/UpdateSection.kt
@@ -6,8 +6,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import app.kreate.android.BuildConfig
 import app.kreate.android.Preferences
@@ -23,15 +21,13 @@ import me.knighthat.updater.Updater
 fun LazyListScope.updateSection( search: SettingEntrySearch ) = section(R.string.update) {
     if ( !(search appearsIn R.string.update) ) return@section
 
-    var checkUpdateState by Preferences.CHECK_UPDATE
-
     SettingComponents.EnumEntry(
         preference = Preferences.CHECK_UPDATE,
         titleId = R.string.setting_entry_update_checker,
-        subtitle = stringResource( checkUpdateState.subtitleId, BuildConfig.APP_NAME ),
+        subtitle = stringResource( Preferences.CHECK_UPDATE.value.subtitleId, BuildConfig.APP_NAME ),
         trailingContent = {
             AnimatedVisibility(
-                visible = checkUpdateState == CheckUpdateState.ASK,
+                visible = Preferences.CHECK_UPDATE.either( CheckUpdateState.DISABLED ),
                 // Slide in from right + fade in effect.
                 enter = slideInHorizontally(initialOffsetX = { it }) + fadeIn(initialAlpha = 0f),
                 // Slide out from left + fade out effect.
