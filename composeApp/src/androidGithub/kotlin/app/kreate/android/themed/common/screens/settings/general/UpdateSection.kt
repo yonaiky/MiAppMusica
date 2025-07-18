4dbc29562185dd159925e1b6040d98b4fa7cd05b
knighthat <git@knighthat.me>
2025-07-17 20:23:19 -0500

    add setting entry to view changelogs

--- composeApp/src/androidGithub/kotlin/app/kreate/android/themed/common/screens/settings/general/UpdateSection.kt
+++ composeApp/src/androidGithub/kotlin/app/kreate/android/themed/common/screens/settings/general/UpdateSection.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.runtime.remember
 import androidx.compose.ui.res.stringResource
 import app.kreate.android.BuildConfig
 import app.kreate.android.Preferences
@@ -15,29 +16,37 @@ import app.kreate.android.themed.common.component.settings.SettingEntrySearch
 import app.kreate.android.themed.common.component.settings.section
 import it.fast4x.rimusic.enums.CheckUpdateState
 import it.fast4x.rimusic.ui.components.themed.SecondaryTextButton
+import me.knighthat.updater.ChangelogsDialog
 import me.knighthat.updater.Updater
 
 
 fun LazyListScope.updateSection( search: SettingEntrySearch ) = section(R.string.update) {
-    if ( !(search appearsIn R.string.update) ) return@section
-
-    SettingComponents.EnumEntry(
-        preference = Preferences.CHECK_UPDATE,
-        titleId = R.string.setting_entry_update_checker,
-        subtitle = stringResource( Preferences.CHECK_UPDATE.value.subtitleId, BuildConfig.APP_NAME ),
-        trailingContent = {
-            AnimatedVisibility(
-                visible = Preferences.CHECK_UPDATE.either( CheckUpdateState.DISABLED ),
-                // Slide in from right + fade in effect.
-                enter = slideInHorizontally(initialOffsetX = { it }) + fadeIn(initialAlpha = 0f),
-                // Slide out from left + fade out effect.
-                exit = slideOutHorizontally(targetOffsetX = { it }) + fadeOut(targetAlpha = 0f)
-            ) {
-                SecondaryTextButton(
-                    text = stringResource( R.string.info_check_update_now ),
-                    onClick = { Updater.checkForUpdate( true ) }
-                )
+    if ( search appearsIn R.string.update )
+        SettingComponents.EnumEntry(
+            preference = Preferences.CHECK_UPDATE,
+            titleId = R.string.setting_entry_update_checker,
+            subtitle = stringResource( Preferences.CHECK_UPDATE.value.subtitleId, BuildConfig.APP_NAME ),
+            trailingContent = {
+                AnimatedVisibility(
+                    visible = Preferences.CHECK_UPDATE.either( CheckUpdateState.DISABLED ),
+                    // Slide in from right + fade in effect.
+                    enter = slideInHorizontally(initialOffsetX = { it }) + fadeIn(initialAlpha = 0f),
+                    // Slide out from left + fade out effect.
+                    exit = slideOutHorizontally(targetOffsetX = { it }) + fadeOut(targetAlpha = 0f)
+                ) {
+                    SecondaryTextButton(
+                        text = stringResource( R.string.info_check_update_now ),
+                        onClick = { Updater.checkForUpdate( true ) }
+                    )
+                }
             }
-        }
+        )
+
+    val changelogs = remember { ChangelogsDialog() }
+    changelogs.Render()
+
+    SettingComponents.Text(
+        title = stringResource( R.string.setting_entry_view_changelogs ),
+        onClick = changelogs::showDialog
     )
 }
