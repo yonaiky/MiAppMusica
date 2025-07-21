ed97e376def3282b3b4c5a06f3c195e75fb41032
knighthat <git@knighthat.me>
2025-07-21 02:14:22 -0500

    fix thumbnail didn't show up in Discord RPC
    Thanks to NEVARLeVrai's suggestion: https://github.com/NEVARLeVrai/N-Zik/blob/main/composeApp/src/androidMain/kotlin/it/fast4x/rimusic/extensions/discord/DiscordPresenceManager.kt

--- composeApp/src/androidMain/kotlin/it/fast4x/rimusic/extensions/discord/Discord.kt
+++ composeApp/src/androidMain/kotlin/it/fast4x/rimusic/extensions/discord/Discord.kt
@@ -1,6 +1,10 @@
 package it.fast4x.rimusic.extensions.discord
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Build
 import android.view.ViewGroup
 import android.webkit.CookieManager
@@ -20,6 +24,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.scale
+import androidx.core.net.toFile
 import androidx.media3.common.MediaItem
 import app.kreate.android.BuildConfig
 import app.kreate.android.Preferences
@@ -28,15 +34,31 @@ import com.my.kizzyrpc.KizzyRPC
 import com.my.kizzyrpc.model.Activity
 import com.my.kizzyrpc.model.Assets
 import com.my.kizzyrpc.model.Timestamps
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import it.fast4x.innertube.Innertube
 import it.fast4x.rimusic.LocalPlayerAwareWindowInsets
-import it.fast4x.rimusic.appContext
 import it.fast4x.rimusic.cleanPrefix
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.add
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.putJsonArray
 import me.knighthat.innertube.Constants
 import me.knighthat.utils.Repository
 import me.knighthat.utils.Toaster
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.jetbrains.annotations.Contract
+import java.io.File
+import java.io.FileOutputStream
 
 @SuppressLint("SetJavaScriptEnabled")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -154,6 +176,130 @@ fun DiscordLoginAndGetToken( onDone: () -> Unit ) {
     }
 }
 
+//<editor-fold desc="Awesome adaptation to make song's thumbnail to show up in RPC. Thanks to NEVARLeVrai">
+// https://github.com/NEVARLeVrai/N-Zik
+private const val APPLICATION_ID = "1370148610158759966"
+
+private fun compressImage(file: File, maxSize: Int = 512): File? {
+    return try {
+        val options = BitmapFactory.Options().apply { inJustDecodeBounds = false }
+        var bitmap = BitmapFactory.decodeFile(file.absolutePath, options)
+
+        if (bitmap.width > maxSize || bitmap.height > maxSize) {
+            val ratio = minOf(maxSize.toFloat() / bitmap.width, maxSize.toFloat() / bitmap.height)
+            val width = (bitmap.width * ratio).toInt()
+            val height = (bitmap.height * ratio).toInt()
+            bitmap = bitmap.scale(width, height)
+        }
+
+        val compressedFile = File.createTempFile("compressed_", ".jpg", file.parentFile)
+        FileOutputStream(compressedFile).use { out ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 5, out)
+        }
+        compressedFile
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
+
+const val TMP_FILES_URL = "https://tmpfiles.org/api/v1/upload"
+
+private suspend fun uploadArtwork( artworkUri: Uri ): String? {
+    val file = artworkUri.toFile()
+    val compressed = if (file.length() > 1_000_000) compressImage(file) else file
+
+    try {
+        val body: MultipartBody = MultipartBody.Builder()
+                                               .setType(MultipartBody.FORM)
+                                               .addFormDataPart(
+                                                   name = "file",
+                                                   filename = compressed!!.name,
+                                                   body = compressed.readBytes()
+                                                       .toRequestBody("image/*".toMediaType())
+                                               )
+                                               .build()
+        return Innertube.client
+                        .post( TMP_FILES_URL ) {
+                            setBody( body )
+                        }
+                        .body<JsonObject>()["data"]
+                        ?.jsonObject["url"]
+                        ?.jsonPrimitive
+                        ?.content
+                        ?.replace(
+                            "https://tmpfiles.org/",
+                            "https://tmpfiles.org/dl/"
+                        )
+    } catch (e: Exception) {
+        e.printStackTrace()
+        e.message?.also( Toaster::e )
+
+        return null
+    } finally {
+        if (compressed != null && compressed != file) compressed.delete()
+    }
+}
+
+private fun isLocalArtwork( artworkUri: Uri ): Boolean =
+    when( artworkUri.scheme ) {
+        "file", "content" -> artworkUri.toFile().exists()
+        else              -> false
+    }
+
+private suspend fun getDiscordAssetUri( imageUrl: String, token: String ): String? {
+    if ( imageUrl.startsWith( "mp:" ) ) return imageUrl
+
+    return runCatching {
+        Innertube.client
+                 .post( "https://discord.com/api/v9/applications/$APPLICATION_ID/external-assets" ) {
+                     headers.append( "Authorization", token )
+                     setBody(
+                         // Use this to ensure syntax
+                         // {"urls":[imageUrl]}
+                         buildJsonObject {
+                             putJsonArray( "urls" ) { add( imageUrl ) }
+                         }
+                     )
+                 }
+                 .body<JsonArray>()
+                 .firstOrNull()
+                 ?.jsonObject["external_asset_path"]
+                 ?.jsonPrimitive
+                 ?.content
+                 ?.let { "mp:$it" }
+    }.onFailure {
+        it.printStackTrace()
+        it.message?.also( Toaster::e )
+    }.getOrNull()
+}
+
+private lateinit var smallImage: String
+
+@Contract("_,null->null")
+private suspend fun getLargeImageUrl( token: String, artworkUri: Uri? ): String? {
+    if( artworkUri == null ) return null
+
+    val isLocalArtwork = isLocalArtwork( artworkUri )
+    val onlineArtworkUri = if( isLocalArtwork )
+        uploadArtwork( artworkUri )
+    else
+        artworkUri.toString().takeIf { it.startsWith("http") }
+
+    return if ( onlineArtworkUri != null )
+        getDiscordAssetUri(onlineArtworkUri, token)
+    else
+        getSmallImageUrl( token )
+}
+
+private suspend fun getSmallImageUrl( token: String ): String? =
+    if ( ::smallImage.isInitialized )
+        smallImage
+    else
+        getDiscordAssetUri("https://i.ibb.co/3mLGkPwY/app-logo.png", token)
+            ?.also { smallImage = it }
+//</editor-fold>
+
 private lateinit var rpc: KizzyRPC
 private lateinit var activity: Activity
 
@@ -172,13 +318,13 @@ fun updateDiscordPresence(
     if( !::rpc.isInitialized || rpc.token != token ) {
         rpc = KizzyRPC( token )
         activity = Activity(
-            applicationId = "1370148610158759966",
+            applicationId = APPLICATION_ID,
             name = BuildConfig.APP_NAME,
             type = TypeDiscordActivity.LISTENING.value,
             buttons = listOf(
-                appContext().getString( R.string.get_app, BuildConfig.APP_NAME ),
-                appContext().getString( R.string.listen_on_youtube )
-            ),
+                context.getString( R.string.get_app, BuildConfig.APP_NAME ),
+                context.getString( R.string.listen_on_youtube )
+            )
         )
     }
 
@@ -188,8 +334,8 @@ fun updateDiscordPresence(
             state = cleanPrefix( mediaItem.mediaMetadata.artist.toString() ),
             timestamps = Timestamps(timeEnd, timeStart),
             assets = Assets(
-                largeImage = mediaItem.mediaMetadata.artworkUri.toString(),
-                smallImage = "https://i.ibb.co/0yd8QzkV/app-logo.png",
+                largeImage = getLargeImageUrl( token, mediaItem.mediaMetadata.artworkUri ),
+                smallImage = getSmallImageUrl( token ),
             ),
             metadata = com.my.kizzyrpc.model.Metadata(
                 listOf(
