19b0a657a7ce1dbd099cee57979a209d23f0c0f4
knighthat <git@knighthat.me>
2025-07-30 00:29:32 -0500

    add option to compress request

--- composeApp/src/androidMain/kotlin/app/kreate/android/service/innertube/InnertubeProvider.kt
+++ composeApp/src/androidMain/kotlin/app/kreate/android/service/innertube/InnertubeProvider.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import app.kreate.android.Preferences
 import app.kreate.android.service.NetworkService
+import io.ktor.client.plugins.compression.compress
 import io.ktor.client.request.accept
 import io.ktor.client.request.headers
 import io.ktor.client.request.request
@@ -100,6 +101,8 @@ class InnertubeProvider: Innertube.Provider {
                           .also { sapisidHash = it }
                 append("Authorization", "SAPISIDHASH ${currentTime}_$sapisidHash")
             }
+
+            compress( "gzip" )
         }
 
         Response(
