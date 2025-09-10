fc7a80a41b2d61c5c159d35d76b9f02788bdc68b
knighthat <git@knighthat.me>
2025-09-10 12:23:41 -0500

    Add phone's information to crash report

--- composeApp/src/androidMain/kotlin/app/kreate/android/utils/CrashHandler.kt
+++ composeApp/src/androidMain/kotlin/app/kreate/android/utils/CrashHandler.kt
@@ -2,6 +2,7 @@ package app.kreate.android.utils
 
 import android.app.Activity
 import android.content.Context
+import android.os.Build
 import androidx.core.net.toUri
 import app.kreate.android.BuildConfig
 import me.knighthat.utils.TimeDateUtils
@@ -45,8 +46,23 @@ class CrashHandler(
             logFile.createNewFile()
 
         context.contentResolver.openOutputStream( logFile.toUri() )?.use { outStream ->
+            val writer = PrintWriter(outStream)
+
+            writer.println( "Version: ${BuildConfig.VERSION_NAME}" )
+            writer.println( "Manufacturer: ${Build.MANUFACTURER}" )
+            writer.println( "Model: ${Build.MODEL}" )
+            writer.println( "Brand: ${Build.BRAND}" )
+            writer.println( "Device: ${Build.DEVICE}" )
+            writer.println( "Product: ${Build.PRODUCT}" )
+            writer.println( "Hardware: ${Build.HARDWARE}" )
+            writer.println( "SDK: ${Build.VERSION.SDK_INT}" )
+            writer.println( "Release: ${Build.VERSION.RELEASE}" )
+            writer.println( "Build Incremental: ${Build.VERSION.INCREMENTAL}" )
+            writer.println()
             // Similar to [Throwable.stackTraceToString], but writing to [outStream] instead
-            PrintWriter(outStream).also( e::printStackTrace ).flush()
+            e.printStackTrace( writer )
+
+            writer.flush()
         }
 
         (context as? Activity)?.finishAndRemoveTask()
